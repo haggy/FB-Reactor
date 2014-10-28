39197bc659b142e3f03bc6043625d0149f0eb9ca
Dave Hagman <haggy102@gmail.com>
2014-10-28 02:53:32 -0400

    Remove local reactorfile

--- reactorfile.js
+++
@@ -1,8 +0,0 @@
-{
-  "app_root": "/src",
-  "component_root": "/src/components",
-  "action_root": "/src/actions",
-  "store_root": "/src/stores",
-  "mixin_root": "/src/mixins",
-  "lib_root": "/src/libs"
-}
